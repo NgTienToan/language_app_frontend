ff4699e9a1af8171868188a2d99e9c7b9ba3e8c9
Ngọc Hải Nguyễn <ngochai220998@gmail.com>
2020-09-16 15:20:39 +0700

    Init project setup [ENV SCSS ESLINT i18n API ...]

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">Nguyen Ngoc Hai {{ $t("welcome") }}</h1>
+      <nuxt-link :to="localePath('/login')">
+        <a-button type="danger"> Danger </a-button>
+      </nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
